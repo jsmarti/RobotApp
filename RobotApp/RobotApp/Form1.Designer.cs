d3bcba35b8065b5504feee2b890411b2bec4e0d4
jsmarti <jsmarti@hotmail.com>
2015-04-09 15:00:39 -0700

    Adición de logs
    Se adhieren los logs de comandos y del robot

--- RobotApp/RobotApp/Form1.Designer.cs
+++ RobotApp/RobotApp/Form1.Designer.cs
@@ -29,11 +29,46 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.commandLog = new System.Windows.Forms.TextBox();
+            this.robotLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // commandLog
+            // 
+            this.commandLog.Location = new System.Drawing.Point(589, 50);
+            this.commandLog.Multiline = true;
+            this.commandLog.Name = "commandLog";
+            this.commandLog.Size = new System.Drawing.Size(309, 118);
+            this.commandLog.TabIndex = 0;
+            // 
+            // robotLog
+            // 
+            this.robotLog.Location = new System.Drawing.Point(589, 197);
+            this.robotLog.Multiline = true;
+            this.robotLog.Name = "robotLog";
+            this.robotLog.Size = new System.Drawing.Size(309, 118);
+            this.robotLog.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(936, 445);
+            this.Controls.Add(this.robotLog);
+            this.Controls.Add(this.commandLog);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox commandLog;
+        private System.Windows.Forms.TextBox robotLog;
     }
 }
 
